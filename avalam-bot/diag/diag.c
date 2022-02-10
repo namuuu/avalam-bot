025273f19aab82722e0e1f2af35bb507dd29dcc1
AugustinH1 <98890365+AugustinH1@users.noreply.github.com>
2022-02-10 16:39:01 +0100

    Add files via upload

--- avalam-bot/diag/diag.c
+++ avalam-bot/diag/diag.c
@@ -1,130 +1,141 @@
-#include <diag.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <avalam.h>
+#include <topologie.h>
+#include <string.h>
 
-char *str_replace(char *orig, char *rep, char *with);
+#ifdef __DEBUG__
+	#define affpos afficherPosition(p)
+	#define cheminjs puts(chemin)
+#else
+	#define affpos
+	#define cheminjs
+#endif
 
-int main(int argc , char * argv[]) {
-
-    if(argc == 1) {
-        printf(RED "Mauvais arguments ! \n "RESET"Arguments : <diag> <fen>\n");
-        return 1;
-    }
-
-    printf("Diagramme: "YEL"%s\n"RESET, argv[1]);
-	printf("Fen: "YEL"%s"RESET"\n", argv[2]);
-
-    FILE * fp, * fp2;
-	int i,trait;
-	char ch;
-
-    writeJS(argv[1], argv[2]);
-}
-
-int writeJS(char diag[], char fen[]) {
-    FILE * fp;
-
-    char save[500];
-    strcpy(save, path);
-    strcat(save, filename);
-    strcat(save, ".js");
-
-    fp = fopen(save, "w+");
-
-    fputs("traiterJson({\n", fp);
-
-    fputs(STR_TURN":", fp);
-	fprintf(fp, "%d,\n", ((strcmp(&fen[strlen(fen)-1],"r") == 0) ? 2 : 1));
-
-    //put number of diag in the js doc
-
-	fputs(STR_NUMDIAG":", fp);
-	fprintf(fp, "%s,\n", diag);
+//void writeJSON(T_Position p,  T_Score score, char *chemin,char *argv[], char descrition[]);
 
 
 
-	//put comment of diag in the js doc
+int main(int argc, char * argv[]){
 
-	fputs(STR_NOTES":\"", fp);
+	char chemin[UNKNOWN]="diag.js";
+	char tmp[UNKNOWN]="../web/data/";
 
-    formatJS(fp);
+
+	if(argc==1){
+		printf("diag <numDiag> <fen>\n");
+		return 0;
+	}
+
 	
-	fputs("\",\n", fp);
+	
+	printf("Diagramme %s\n", argv[1]);
+	printf("Fen : %s\n", argv[2]);
 
 
-	//put FEN of diag in the js doc
-	fputs(STR_FEN":\"", fp);
-	fprintf(fp, "%s\",\n", fen);
+	//changement de nom
+	printf("Fichier (sera créé dans le répertoire ./web/data s'il existe) ? [diag.js]");
+	
+	fgets(chemin, UNKNOWN, stdin);
 
-    fclose(fp);
+	if (strcasecmp(chemin,"\n") == 0)
+	{
+		strcpy(chemin,"../web/data/diag.js");
+		puts(chemin);
+	}
+	else
+	{
+		chemin[strlen(chemin)-1] = '\0';
+		strcat(tmp,chemin);
+		strcpy(chemin,tmp);
+	}
+	
+
+	
+
+
+
+
+
+	FILE * fp;
+
+	fp = fopen(chemin, "w+");
+	fputs("coucou", fp);
+
+
+
+	/*
+	if(strcmp(&argv[2][strlen(argv[2])-1],"r") == 0) trait = 2;else trait = 1;
+	*/
+
+
+	fclose(fp);
+
 }
 
-int formatJS(FILE * fp) {
-    printf("Description (vous pouvez saisir du HTML, Ctrl+D pour terminer) ? []"YEL"\n");
-    system("cat > desc");
 
-    FILE * fp2;
-	//copy
-	// ouvrir le fichier en lecture
-	fp2 = fopen("desc", "r");
+/*	fonction de création de json
+void writeJSON(T_Position p, T_Score score, char *chemin ,char *argv[], char description[]) {
 
-	// Lire le contenu du fichier
 
-    char notes[500];
-    while (fgets(notes, 500, fp2) != NULL) {
-        strcpy(notes, str_replace(notes, "\n", "<br>"));
-        fputs(notes, fp);
-    }
 
-	fclose(fp2);
+	
+	FILE * fp;
+	
+	fp = fopen(chemin, "w+");
+	fputs("traiterJson({\n", fp);
 
-    system("rm desc");
-	//paste
+	
+	
+	fputs(STR_TURN":", fp);
+	fprintf(fp, "%d,\n", p.trait);
+	
 
-    return 0;
-}
+    fputs(STR_NUMDIAG":", fp);
+	fprintf(fp, "%s,\n", argv[1]);
 
-// You must free the result if result is non-NULL.
-char *str_replace(char *orig, char *rep, char *with) {
-    char *result; // the return string
-    char *ins;    // the next insert point
-    char *tmp;    // varies
-    int len_rep;  // length of rep (the string to remove)
-    int len_with; // length of with (the string to replace rep with)
-    int len_front; // distance between rep and end of last rep
-    int count;    // number of replacements
 
-    // sanity checks and initialization
-    if (!orig || !rep)
-        return NULL;
-    len_rep = strlen(rep);
-    if (len_rep == 0)
-        return NULL; // empty rep causes infinite loop during count
-    if (!with)
-        with = "";
-    len_with = strlen(with);
+    fputs(STR_NOTES":", fp);
+	fprintf(fp, "\"%s\",\n", description);
 
-    // count the number of replacements needed
-    ins = orig;
-    for (count = 0; tmp = strstr(ins, rep); ++count) {
-        ins = tmp + len_rep;
-    }
 
-    tmp = result = malloc(strlen(orig) + (len_with - len_rep) * count + 1);
 
-    if (!result)
-        return NULL;
 
-    // first time through the loop, all the variable are set correctly
-    // from here on,
-    //    tmp points to the end of the result string
-    //    ins points to the next occurrence of rep in orig
-    //    orig points to the remainder of orig after "end of rep"
-    while (count--) {
-        ins = strstr(orig, rep);
-        len_front = ins - orig;
-        tmp = strncpy(tmp, orig, len_front) + len_front;
-        tmp = strcpy(tmp, with) + len_with;
-        orig += len_front + len_rep; // move to next "end of rep"
-    }
-    strcpy(tmp, orig);
-    return result;
-}
+
+	fputs(STR_SCORE_J":", fp);
+	fprintf(fp, "%d,\n", score.nbJ);
+	
+	fputs(STR_SCORE_J5":", fp);
+	fprintf(fp, "%d,\n", score.nbJ5);
+	
+	fputs(STR_SCORE_R":", fp);
+	fprintf(fp, "%d,\n", score.nbR);
+	
+	fputs(STR_SCORE_R5":", fp);
+	fprintf(fp, "%d,\n", score.nbR5);
+	
+	fputs(STR_BONUS_J":", fp);
+	fprintf(fp, "%d,\n", p.evolution.bonusJ);
+	
+	fputs(STR_MALUS_J":", fp);
+	fprintf(fp, "%d,\n", p.evolution.malusJ);
+	
+	fputs(STR_BONUS_R":", fp);
+	fprintf(fp, "%d,\n", p.evolution.bonusR);
+	
+	fputs(STR_MALUS_R":", fp);
+	fprintf(fp, "%d,\n", p.evolution.malusR);
+	
+	fputs(STR_COLS":[\n", fp);
+
+	
+	for(int i = 0; i < NBCASES-1; i++)
+	{
+		fprintf(fp, "\t{"STR_NB":%d, "STR_COULEUR":%d},\n", p.cols[i].nb,p.cols[i].couleur);
+	}
+	fprintf(fp, "\t{"STR_NB":%d, "STR_COULEUR":%d}\n]\n});", p.cols[NBCASES-1].nb,p.cols[NBCASES-1].couleur);
+	
+	
+	fclose(fp); 
+
+}*/
