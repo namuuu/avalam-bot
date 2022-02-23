22c2025a51c97ac11eb4dd493eec774c26780224
Nawel18 <98890486+Nawel18@users.noreply.github.com>
2022-02-23 14:29:02 +0100

    Add files via upload

--- avalam-bot/diag/diag.c
+++ avalam-bot/diag/diag.c
@@ -1,4 +1,19 @@
+/**************************************************************************/
+/*Fichier: diag.c							   	*/
+/*NOM :Robert Arthur Merabet Nawel Husson Augustin David Leevan				 						*/
+/*GROUPE: E7  */
+/* Version : 1.0									*/
+/*Date : 09/02/2022							   		*/
+/***************************************************************************/
 #include <diag.h>
+/***************************************************************************/
+/* NOM FONCTION : fonction principale					           */
+/* DESCRIPTION : Dans cette fonction, l'utilisateur saisi des arguments dans le terminale et cette fonction a pour but de récupérer les arguments pour les utiliser et les transformer */
+/* Retourne :  0       			   */
+/* Effets de bords : Saisie au clavier                                     */
+/* Parametres en entree :int argc, char * argv[]				   */
+/* Parametres en sortie :  entier                                                */
+/***************************************************************************/
 
 
 
@@ -33,144 +48,150 @@ int main(int argc, char * argv[]){
 	
 	readFEN(&p,argv[2], argv[3]);
 
-    writeJS(p, evaluerScore(p), notes, argv[1]);
-	printf("dernier getchar\n");
+    writeJS(p, evaluerScore(p), notes, argv[1]);//écrit le FEN dans le fichier JS
+	//printf("dernier getchar\n");
 	getchar();
 }
+/***************************************************************************/
+/* NOM FONCTION : readFEN					           */
+/* DESCRIPTION : Il transforme le deuxième argument en placement dans le plateau       */
+/* Retourne : 0     			   */
+/* Parametres en entree :T_Position *p, char argv[], char trait[]						   */
+/* Parametres en sortie : entier                                                */
+/***************************************************************************/
+
 
 int readFEN(T_Position *p, char argv[], char trait[]) {
     int i=0,j=0,k=0;
-  	// printf("a=%c",argv[0]);
-
-    
     for(i=0;i<48;i++){
         p->cols[i].nb=0;
         p->cols[i].couleur=0;
     }
-    
+    //intialision des bonus et des malus à -1
     p->evolution.bonusJ=-1;
     p->evolution.bonusR=-1;
     p->evolution.malusJ=-1;
     p->evolution.malusR=-1;
     i=0;
     if(argv!=NULL){
-    while(argv[j]!='\0') {
-        
-       //j=j+1;
-		switch(argv[j]) { 
-			case 'u' : 
-			p->cols[i].nb=1;
-			p->cols[i].couleur=1;
-			i++;
-			break;
-			case 'd':
-			p->cols[i].nb=2;
-			p->cols[i].couleur=1;
-			i++;
-			break;
-			case 't':
-			p->cols[i].nb=3;
-			p->cols[i].couleur=1;
-			i++;
-			break;
-			case 'q':
-			p->cols[i].nb=4;
-			p->cols[i].couleur=1;
-			i++;
-			break;
-			case 'c': 
-			p->cols[i].nb=5;
-			p->cols[i].couleur=1;
-			i++;
-			break;
-			case 'U': 
-			p->cols[i].nb=1;
-			p->cols[i].couleur=2;
-			i++;
-			break;
-			case 'D':
-			p->cols[i].nb=2;
-			p->cols[i].couleur=2;
-			i++;
-			break;
-			case 'T': 
-			p->cols[i].nb=3;
-			p->cols[i].couleur=2;
-			i++;
-			break;
-			case 'Q':
-			p->cols[i].nb=4;
-			p->cols[i].couleur=2;
-			i++; 
-			break;
-			case 'C':
-			p->cols[i].nb=5;
-			p->cols[i].couleur=2;
-			i++;
-			break;
-			case 'b':
-			if(p->evolution.bonusJ==-1){
-				p->evolution.bonusJ=i+1;
-			}
-			break;
-			case 'B':
-			if(p->evolution.bonusR==-1){
-				p->evolution.bonusR=i+1;
-			}
-			break;
-			case 'm':
-			if(p->evolution.malusJ==-1){
-				p->evolution.malusJ=i+1;
-			}
-			break;
-			case 'M':
-			if(p->evolution.malusR==-1){
-				p->evolution.malusR=i+1;
-			}
-			break;
-			case '0'...'9':
-			if(argv[j+1]>'0'&& argv[j+1]<'9' ){
+		while(argv[j]!='\0') {
+			switch(argv[j]) { //Switch pour les différentes possibilitées
+				case 'u' : 
+				p->cols[i].nb=1;//la taille de la pile
+				p->cols[i].couleur=1;//la couleur
+				i++;
+				break;
+				case 'd':
+				p->cols[i].nb=2;
+				p->cols[i].couleur=1;
+				i++;
+				break;
+				case 't':
+				p->cols[i].nb=3;
+				p->cols[i].couleur=1;
+				i++;
+				break;
+				case 'q':
+				p->cols[i].nb=4;
+				p->cols[i].couleur=1;
+				i++;
+				break;
+				case 'c': 
+				p->cols[i].nb=5;
+				p->cols[i].couleur=1;
+				i++;
+				break;
+				case 'U': 
+				p->cols[i].nb=1;
+				p->cols[i].couleur=2;
+				i++;
+				break;
+				case 'D':
+				p->cols[i].nb=2;
+				p->cols[i].couleur=2;
+				i++;
+				break;
+				case 'T': 
+				p->cols[i].nb=3;
+				p->cols[i].couleur=2;
+				i++;
+				break;
+				case 'Q':
+				p->cols[i].nb=4;
+				p->cols[i].couleur=2;
+				i++; 
+				break;
+				case 'C':
+				p->cols[i].nb=5;
+				p->cols[i].couleur=2;
+				i++;
+				break;
+				case 'b':
+				if(p->evolution.bonusJ==-1){
+					p->evolution.bonusJ=i+1;
+				}
+				break;
+				case 'B':
+				if(p->evolution.bonusR==-1){
+					p->evolution.bonusR=i+1;
+				}
+				break;
+				case 'm':
+				if(p->evolution.malusJ==-1){
+					p->evolution.malusJ=i+1;
+				}
+				break;
+				case 'M':
+				if(p->evolution.malusR==-1){
+					p->evolution.malusR=i+1;
+				}
+				break;
+				case '0'...'9':
+				if(argv[j+1]>'0'&& argv[j+1]<'9' ){
 
-					int r= (argv[j] - '0')*10  + (argv[j+1] - '0');
-					for(int k=0;k<=r;k++){
+						int r= (argv[j] - '0')*10  + (argv[j+1] - '0');
+						for(int k=0;k<=r;k++){
+							p->cols[i+k].couleur = 0;
+							p->cols[i+k].nb = 0;
+						}
+						j=j+1;
+						
+						while(argv[j+1]>'0'&& argv[j+1]<'9')
+						{
+							j=j+1;
+						}
+						i=i+r+1;
+				}else{
+
+					int r= (argv[j] - '0');
+					for(int k=0; k<=r; k++) {
 						p->cols[i+k].couleur = 0;
 						p->cols[i+k].nb = 0;
 					}
-					j=j+1;
-					
-					while(argv[j+1]>'0'&& argv[j+1]<'9')
-					{
-						j=j+1;
-					}
 					i=i+r+1;
-			}else{
-
-				int r= (argv[j] - '0');
-				for(int k=0; k<=r; k++) {
-					p->cols[i+k].couleur = 0;
-					p->cols[i+k].nb = 0;
 				}
-				i=i+r+1;
+				break;
 			}
-			
-			printf("i=%d\n",i);
-			break;
+		j=j+1;
     	}
-	printf("argv[%d]=%c\n",j,argv[j]);
-  	j=j+1;
-    }
 
-	if(trait[0]=='r') {
-		p->trait=2;
-	} else {
-		p->trait=1;
+		if(trait[0]=='r') {//initialisation du trait
+			p->trait=2;
+		} else {
+			p->trait=1;
+		}
+		return 0;
 	}
-
-	return 0;
-}
 }
 
 //	fonction de création de json
+/***************************************************************************/
+/* NOM FONCTION : writeJS					           */
+/* DESCRIPTION : A l'aide de fputs et fprintf et des variables, on écrit dans le JS.      */
+/* Retourne :rien    			   */
+/* Parametres en entree :T_Position p, T_Score score, char notes[], char diag[]						   */
+/* Parametres en sortie : fichier.js avec le contenue                                               */
+/***************************************************************************/
 void writeJS(T_Position p, T_Score score, char notes[], char diag[]) {
 
 	FILE * fp;
@@ -235,7 +256,13 @@ void writeJS(T_Position p, T_Score score, char notes[], char diag[]) {
 	fclose(fp); 
 
 }
-
+/***************************************************************************/
+/* NOM FONCTION : readNotes				           */
+/* DESCRIPTION : Cette fonction ouvre un fichier pour écrire le texte donner par l'utilisateur                                              */
+/* Retourne :0         			   */
+/* Parametres en entree :char notes[]						   */
+/* Parametres en sortie :entier                                         */
+/***************************************************************************/
 int readNotes(char notes[]) {
     
     system("cat > desc");
@@ -259,6 +286,13 @@ int readNotes(char notes[]) {
 }
 
 // You must free the result if result is non-NULL.
+/***************************************************************************/
+/* NOM FONCTION : str_replace				           */
+/* DESCRIPTION :                                             */
+/* Retourne :entier : result         			   */
+/* Parametres en entree :char *orig, char *rep, char *with						   */
+/* Parametres en sortie :char *                                           */
+/***************************************************************************/
 char *str_replace(char *orig, char *rep, char *with) {
     char *result; // the return string
     char *ins;    // the next insert point
