1b6b5cdfcb011a4ccdf9c889fb81647e13fb9610
Nawel18 <98890486+Nawel18@users.noreply.github.com>
2022-02-23 15:41:05 +0100

    Add files via upload

--- avalam-bot/diag/diag.c
+++ avalam-bot/diag/diag.c
@@ -6,8 +6,6 @@
 /*Date : 09/02/2022							   		*/
 /***************************************************************************/
 #include <diag.h>
-
-
 /***************************************************************************/
 /* NOM FONCTION : fonction principale					           */
 /* DESCRIPTION : Dans cette fonction, l'utilisateur saisi des arguments dans le terminale et cette fonction a pour but de récupérer les arguments pour les utiliser et les transformer */
@@ -16,69 +14,44 @@
 /* Parametres en entree :int argc, char * argv[]				   */
 /* Parametres en sortie :  entier                                                */
 /***************************************************************************/
+
+
+
 int main(int argc, char * argv[]){
     T_Position p;
 
 	char chemin[UNKNOWN]="diag.js";
 
 	// Stops the program if not enough arguments
-	if(argc<=2){
+	if(argc<=3){
 		printf(RED "Mauvais arguments ! \n "RESET"Arguments : <diag> <fen>\n");
         return 1;
 	}
-	// Check if the Diag is a int or not. If it's not an int, sets the Diag to 0.
-	if(isdigit(argv[1][0])) {
-		strcpy(diag, argv[1]);
-	} else {
-		strcpy(diag, "0");
-	}
 
-	printf("Diagramme : %s\n", diag);
+	printf("Diagramme %s\n", argv[1]);
 	printf("Fen : %s\n", argv[2]);
 
-	char notes[500];
 
-	// Check if there's an Input Redirection
-	if (isatty(STDIN_FILENO)) {
-		printf0(DEBUG"Debug > No redirection Detected");
-        // No redirection detected ; normal procedure
-		// Check for a custom file name.
-		printf("Fichier (sera créé dans le répertoire ./web/data s'il existe) ? [diag.js]\n");
-		
-		fgets(chemin, UNKNOWN, stdin);
-
-		if (strcasecmp(chemin,"\n") != 0) {
-			chemin[strlen(chemin)-1] = '\0';
-			strcpy(filename, chemin);
-		}
-
-		// Make the notes using the ReadNotes function.
-		printf("Description (vous pouvez saisir du HTML, Ctrl+D pour terminer) ? []"YEL"\n");
-		readNotes(notes);
-        
-    } else {
-        // Redirection Detected !
-        char buf[500];
-		// First line, filename
-		fgets(buf, 500 , stdin);
-		strcpy(filename, buf);
-		filename[strlen(buf) - 1] = '\0';
-		// First line of description
-		fgets(buf, 500, stdin);
-		strcpy(notes, buf);
-		notes[strlen(buf) - 1] = '\0';
-		strcat(notes, "<br>");
-		// Second line of description
-		fgets(buf, 500, stdin);
-		strcat(notes, buf);
-		printf2(DEBUG"DEBUG > Redirection detected ! \nFilename: %sNotes: %s\n", filename, notes);
-    }
+	//changement de nom
+	printf("Fichier (sera créé dans le répertoire ./web/data s'il existe) ? [diag.js]\n");
 	
-	readFEN(&p,argv[2]);
+	fgets(chemin, UNKNOWN, stdin);
 
-    writeJS(p, evaluerScore(p), argv[2] , notes, diag);
+	if (strcasecmp(chemin,"\n") != 0) {
+		chemin[strlen(chemin)-1] = '\0';
+		strcpy(filename, chemin);
+	}
+
+	printf("Description (vous pouvez saisir du HTML, Ctrl+D pour terminer) ? []"YEL"\n");
+	char notes[500];
+	readNotes(notes);
+
+	readFEN(&p,argv[2], argv[3]);
+
+    writeJS(p, evaluerScore(p), notes, argv[1]);//écrit le FEN dans le fichier JS
+	//printf("dernier getchar\n");
+	getchar();
 }
-
 /***************************************************************************/
 /* NOM FONCTION : readFEN					           */
 /* DESCRIPTION : Il transforme le deuxième argument en placement dans le plateau       */
@@ -86,139 +59,136 @@ int main(int argc, char * argv[]){
 /* Parametres en entree :T_Position *p, char argv[], char trait[]						   */
 /* Parametres en sortie : entier                                                */
 /***************************************************************************/
-int readFEN(T_Position *p, char argv[]) {
-    int i=0,j=0,k=0;
 
-    // Sets the whole board to an empty one.
+
+int readFEN(T_Position *p, char argv[], char trait[]) {
+    int i=0,j=0,k=0;
     for(i=0;i<48;i++){
         p->cols[i].nb=0;
         p->cols[i].couleur=0;
     }
-    
-	// Sets the evolution points to their default values
-    p->evolution.bonusJ=255;
-    p->evolution.bonusR=255;
-    p->evolution.malusJ=255;
-    p->evolution.malusR=255;
-	
+    //intialision des bonus et des malus à -1
+    p->evolution.bonusJ=-1;
+    p->evolution.bonusR=-1;
+    p->evolution.malusJ=-1;
+    p->evolution.malusR=-1;
     i=0;
-
     if(argv!=NULL){
-    while(argv[j]!='\0') {
-        
-		switch(argv[j]) { 
-			case 'u' : 
-			p->cols[i].nb=1;
-			p->cols[i].couleur=1;
-			i++;
-			break;
-			case 'd':
-			p->cols[i].nb=2;
-			p->cols[i].couleur=1;
-			i++;
-			break;
-			case 't':
-			p->cols[i].nb=3;
-			p->cols[i].couleur=1;
-			i++;
-			break;
-			case 'q':
-			p->cols[i].nb=4;
-			p->cols[i].couleur=1;
-			i++;
-			break;
-			case 'c': 
-			p->cols[i].nb=5;
-			p->cols[i].couleur=1;
-			i++;
-			break;
-			case 'U': 
-			p->cols[i].nb=1;
-			p->cols[i].couleur=2;
-			i++;
-			break;
-			case 'D':
-			p->cols[i].nb=2;
-			p->cols[i].couleur=2;
-			i++;
-			break;
-			case 'T': 
-			p->cols[i].nb=3;
-			p->cols[i].couleur=2;
-			i++;
-			break;
-			case 'Q':
-			p->cols[i].nb=4;
-			p->cols[i].couleur=2;
-			i++; 
-			break;
-			case 'C':
-			p->cols[i].nb=5;
-			p->cols[i].couleur=2;
-			i++;
-			break;
-			case 'b':
-			if(p->evolution.bonusJ==255){
-				p->evolution.bonusJ=i-1;
-			}
-			break;
-			case 'B':
-			if(p->evolution.bonusR==255){
-				p->evolution.bonusR=i-1;
-			}
-			break;
-			case 'm':
-			if(p->evolution.malusJ==255){
-				p->evolution.malusJ=i-1;
-			}
-			break;
-			case 'M':
-			if(p->evolution.malusR==255){
-				p->evolution.malusR=i-1;
-			}
-			break;
-			// Case where there is a number, creates
-			case '0'...'9':
-			if(argv[j+1]>'0'&& argv[j+1]<'9' ){
+		while(argv[j]!='\0') {
+			switch(argv[j]) { //Switch pour les différentes possibilitées
+				case 'u' : 
+				p->cols[i].nb=1;//la taille de la pile
+				p->cols[i].couleur=1;//la couleur
+				i++;
+				break;
+				case 'd':
+				p->cols[i].nb=2;
+				p->cols[i].couleur=1;
+				i++;
+				break;
+				case 't':
+				p->cols[i].nb=3;
+				p->cols[i].couleur=1;
+				i++;
+				break;
+				case 'q':
+				p->cols[i].nb=4;
+				p->cols[i].couleur=1;
+				i++;
+				break;
+				case 'c': 
+				p->cols[i].nb=5;
+				p->cols[i].couleur=1;
+				i++;
+				break;
+				case 'U': 
+				p->cols[i].nb=1;
+				p->cols[i].couleur=2;
+				i++;
+				break;
+				case 'D':
+				p->cols[i].nb=2;
+				p->cols[i].couleur=2;
+				i++;
+				break;
+				case 'T': 
+				p->cols[i].nb=3;
+				p->cols[i].couleur=2;
+				i++;
+				break;
+				case 'Q':
+				p->cols[i].nb=4;
+				p->cols[i].couleur=2;
+				i++; 
+				break;
+				case 'C':
+				p->cols[i].nb=5;
+				p->cols[i].couleur=2;
+				i++;
+				break;
+				case 'b':
+				if(p->evolution.bonusJ==255){
+					i=i+1;
+					p->evolution.bonusJ=i;
+				}
+				break;
+				case 'B':
+				if(p->evolution.bonusR==255){
+					i=i+1;
+					p->evolution.bonusR=i;
+				}
+				break;
+				case 'm':
+				if(p->evolution.malusJ==255){
+					i=i+1;
+					p->evolution.malusJ=i;
+				}
+				break;
+				case 'M':
+				if(p->evolution.malusR==255){
+					i=i+1;
+					p->evolution.malusR=i;
+				}
+				break;
+				case '0'...'9':
+				if(argv[j+1]>'0'&& argv[j+1]<'9' ){
 
-					int r= (argv[j] - '0')*10  + (argv[j+1] - '0');
-					for(int k=0;k<=r;k++){
+						int r= (argv[j] - '0')*10  + (argv[j+1] - '0');
+						for(int k=0;k<=r;k++){
+							p->cols[i+k].couleur = 0;
+							p->cols[i+k].nb = 0;
+						}
+						j=j+1;
+						
+						while(argv[j+1]>'0'&& argv[j+1]<'9')
+						{
+							j=j+1;
+						}
+						i=i+r+1;
+				}else{
+
+					int r= (argv[j] - '0');
+					for(int k=0; k<=r; k++) {
 						p->cols[i+k].couleur = 0;
 						p->cols[i+k].nb = 0;
 					}
-					j=j+1;
-					
-					while(argv[j+1]>'0'&& argv[j+1]<'9')
-					{
-						j=j+1;
-					}
 					i=i+r+1;
-			}else{
-
-				int r= (argv[j] - '0');
-				for(int k=0; k<=r; k++) {
-					p->cols[i+k].couleur = 0;
-					p->cols[i+k].nb = 0;
 				}
-				i=i+r+1;
+				break;
 			}
-			break;
+		j=j+1;
     	}
-  	j=j+1;
-    }
 
-	if(argv[strlen(argv) - 1]=='r') {
-		p->trait=2;
-	} else {
-		p->trait=1;
+		if(trait[0]=='r') {//initialisation du trait
+			p->trait=2;
+		} else {
+			p->trait=1;
+		}
+		return 0;
 	}
-
-	affpos;
-
-	return 0;
-}
 }
 
+//	fonction de création de json
 /***************************************************************************/
 /* NOM FONCTION : writeJS					           */
 /* DESCRIPTION : A l'aide de fputs et fprintf et des variables, on écrit dans le JS.      */
@@ -226,15 +196,14 @@ int readFEN(T_Position *p, char argv[]) {
 /* Parametres en entree :T_Position p, T_Score score, char notes[], char diag[]						   */
 /* Parametres en sortie : fichier.js avec le contenue                                               */
 /***************************************************************************/
-void writeJS(T_Position p, T_Score score, char FEN[], char notes[], char diag[]) {
+void writeJS(T_Position p, T_Score score, char notes[], char diag[]) {
 
 	FILE * fp;
 
     char save[500];
     strcpy(save, path);
     strcat(save, filename);
-
-	printf1(DEBUG"Debug > Saving file to %s", save);
+    strcat(save, ".js");
 
     fp = fopen(save, "w+");
 
@@ -252,10 +221,6 @@ void writeJS(T_Position p, T_Score score, char FEN[], char notes[], char diag[])
 
     fputs(STR_NOTES":\"", fp);
 	fprintf(fp, "%s", notes);
-	fputs("\",\n", fp);
-
-	fputs(STR_FEN":\"", fp);
-	fprintf(fp, "%s", FEN);
 	fputs("\",\n", fp);
 
 	fputs(STR_SCORE_J":", fp);
@@ -295,7 +260,6 @@ void writeJS(T_Position p, T_Score score, char FEN[], char notes[], char diag[])
 	fclose(fp); 
 
 }
-
 /***************************************************************************/
 /* NOM FONCTION : readNotes				           */
 /* DESCRIPTION : Cette fonction ouvre un fichier pour écrire le texte donner par l'utilisateur                                              */
@@ -307,16 +271,14 @@ int readNotes(char notes[]) {
     
     system("cat > desc");
 
-	char buf[500];
-
     FILE * fp2;
 	//copy
 	// ouvrir le fichier en lecture
 	fp2 = fopen("desc", "r");
 
 	// Lire le contenu du fichier
-    while (fgets(buf, 500, fp2) != NULL) {
-        strcat(notes, str_replace(buf, "\n", "<br>"));
+    while (fgets(notes, 500, fp2) != NULL) {
+        strcpy(notes, str_replace(notes, "\n", "<br>"));
     }
 
 	fclose(fp2);
@@ -324,11 +286,10 @@ int readNotes(char notes[]) {
     system("rm desc");
 	//paste
 
-	printf("\n");
-
     return 0;
 }
 
+// You must free the result if result is non-NULL.
 /***************************************************************************/
 /* NOM FONCTION : str_replace				           */
 /* DESCRIPTION :                                             */
